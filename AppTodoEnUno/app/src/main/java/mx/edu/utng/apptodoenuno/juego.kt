bbb5cf522c3c2dc676bb8423ef219c54f25c317d
elisa <manzanoelisa609@gmail.com>
2019-10-11 14:47:36 -0500

    Primera

---
+++ AppTodoEnUno/app/src/main/java/mx/edu/utng/apptodoenuno/juego.kt
@@ -0,0 +1,12 @@
+package mx.edu.utng.apptodoenuno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class juego : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_juego)
+    }
+}
